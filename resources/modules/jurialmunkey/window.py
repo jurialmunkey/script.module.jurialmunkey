8cf0ea8ed38a560e600ef1f0e852e89b0673d31f
jurialmunkey <jurialmunkey@kodi.tv>
2023-10-07 12:54:45 +1100

    :broom: Cleanup imports

--- resources/modules/jurialmunkey/window.py
+++ resources/modules/jurialmunkey/window.py
@@ -1,23 +1,25 @@
-from xbmc import Monitor, executebuiltin, getCondVisibility, getInfoLabel
-from xbmcgui import Window, getCurrentWindowId, getCurrentWindowDialogId
-from jurialmunkey.parser import try_type, try_int
+import xbmc
 
 
 DIALOG_ID_EXCLUDELIST = (9999, None)
 
 
 def get_current_window(get_dialog=True):
-    dialog = getCurrentWindowDialogId() if get_dialog else None
-    return dialog if dialog not in DIALOG_ID_EXCLUDELIST else getCurrentWindowId()
+    import xbmcgui
+    dialog = xbmcgui.getCurrentWindowDialogId() if get_dialog else None
+    return dialog if dialog not in DIALOG_ID_EXCLUDELIST else xbmcgui.getCurrentWindowId()
 
 
 def get_property(name, set_property=None, clear_property=False, window_id=None, prefix=None, is_type=None):
+    import xbmcgui
+    from jurialmunkey.parser import try_type
+
     if prefix != -1:
         prefix = prefix or 'TMDbHelper'
         name = f'{prefix}.{name}'
     if window_id == 'current':
         window_id = get_current_window()
-    window = Window(window_id or 10000)  # Fallback to home window id=10000
+    window = xbmcgui.Window(window_id or 10000)  # Fallback to home window id=10000
     ret_property = set_property or window.getProperty(name)
     if clear_property:
         window.clearProperty(name)
@@ -40,7 +42,7 @@ def wait_for_property(name, value=None, set_property=False, poll=1, timeout=10):
     Will set property to value if set_property flag is set. None value clears property.
     Returns True when successful.
     """
-    xbmc_monitor = Monitor()
+    xbmc_monitor = xbmc.Monitor()
     if set_property:
         get_property(name, value) if value else get_property(name, clear_property=True)
     while (
@@ -54,15 +56,15 @@ def wait_for_property(name, value=None, set_property=False, poll=1, timeout=10):
 
 
 def is_visible(window_id):
-    return getCondVisibility(f'Window.IsVisible({window_id})')
+    return xbmc.getCondVisibility(f'Window.IsVisible({window_id})')
 
 
 def close(window_id):
-    return executebuiltin(f'Dialog.Close({window_id})')
+    return xbmc.executebuiltin(f'Dialog.Close({window_id})')
 
 
 def activate(window_id):
-    return executebuiltin(f'ActivateWindow({window_id})')
+    return xbmc.executebuiltin(f'ActivateWindow({window_id})')
 
 
 def _is_base_active(window_id):
@@ -72,8 +74,9 @@ def _is_base_active(window_id):
 
 
 def _is_updating(container_id):
-    is_updating = getCondVisibility(f"Container({container_id}).IsUpdating")
-    is_numitems = try_int(getInfoLabel(f"Container({container_id}).NumItems"))
+    from jurialmunkey.parser import try_int
+    is_updating = xbmc.getCondVisibility(f"Container({container_id}).IsUpdating")
+    is_numitems = try_int(xbmc.getInfoLabel(f"Container({container_id}).NumItems"))
     if is_updating or not is_numitems:
         return True
 
@@ -89,7 +92,7 @@ def wait_until_active(window_id, instance_id=None, poll=1, timeout=30, invert=Fa
     Wait for window ID to open (or to close if invert set to True). Returns window_id if successful.
     Pass instance_id if there is also a base window that needs to be open underneath
     """
-    xbmc_monitor = Monitor()
+    xbmc_monitor = xbmc.Monitor()
     while (
             not xbmc_monitor.abortRequested() and timeout > 0
             and _is_inactive(window_id, invert)
@@ -106,7 +109,7 @@ def wait_until_updated(container_id=9999, instance_id=None, poll=1, timeout=60):
     Wait for container to update. Returns container_id if successful
     Pass instance_id if there is also a base window that needs to be open underneath
     """
-    xbmc_monitor = Monitor()
+    xbmc_monitor = xbmc.Monitor()
     while (
             not xbmc_monitor.abortRequested() and timeout > 0
             and _is_updating(container_id)
